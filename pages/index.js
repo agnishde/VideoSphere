1d1d553945e63ec711e5db6bac14343cbf9a7b33
Agnish <deagnish11@gmail.com>
2024-02-24 05:30:43 +0530

    setup peer.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,5 @@
-import { useSocket } from "@/context/socket"
-import { useEffect } from "react"
-
 export default function Home(){
-  const socket = useSocket()
-
-  useEffect(() =>{
-    socket?.on("connect", () =>{
-      console.log(socket.id);
-    });
-  }, [socket])
-  return(
+  return (
     <h1>Welcome</h1>
   )
 }
